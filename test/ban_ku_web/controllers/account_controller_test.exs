508c687b6b124a0fa4069d22da51f5769e93b5c4
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2020-02-08 17:23:06 -0300

    remove unused tests

--- test/ban_ku_web/controllers/account_controller_test.exs
+++ test/ban_ku_web/controllers/account_controller_test.exs
@@ -2,15 +2,9 @@ defmodule BanKuWeb.AccountControllerTest do
   use BanKuWeb.ConnCase
 
   alias BanKu.Accounts
-  alias BanKu.Accounts.Account
 
   @create_attrs %{
-    balance: 42,
     owner_name: "some owner_name"
-  }
-  @update_attrs %{
-    balance: 43,
-    owner_name: "some updated owner_name"
   }
   @invalid_attrs %{balance: nil, owner_name: nil}
 
@@ -32,61 +26,21 @@ defmodule BanKuWeb.AccountControllerTest do
 
   describe "create account" do
     test "renders account when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.account_path(conn, :create), account: @create_attrs)
+      conn = post(conn, Routes.account_path(conn, :create), @create_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
       conn = get(conn, Routes.account_path(conn, :show, id))
 
       assert %{
                "id" => id,
-               "balance" => 42,
+               "balance" => 100_000,
                "owner_name" => "some owner_name"
              } = json_response(conn, 200)["data"]
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.account_path(conn, :create), account: @invalid_attrs)
+      conn = post(conn, Routes.account_path(conn, :create), @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
-  end
-
-  describe "update account" do
-    setup [:create_account]
-
-    test "renders account when data is valid", %{conn: conn, account: %Account{id: id} = account} do
-      conn = put(conn, Routes.account_path(conn, :update, account), account: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, Routes.account_path(conn, :show, id))
-
-      assert %{
-               "id" => id,
-               "balance" => 43,
-               "owner_name" => "some updated owner_name"
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, account: account} do
-      conn = put(conn, Routes.account_path(conn, :update, account), account: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete account" do
-    setup [:create_account]
-
-    test "deletes chosen account", %{conn: conn, account: account} do
-      conn = delete(conn, Routes.account_path(conn, :delete, account))
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.account_path(conn, :show, account))
-      end
-    end
-  end
-
-  defp create_account(_) do
-    account = fixture(:account)
-    {:ok, account: account}
   end
 end
